c2ff97bc6cd7a368607f70494f42080f82bcdc07
Yuriy Khabarov <13real008@gmail.com>
2019-12-29 16:51:18 +0100

    use document instead of window

--- src/app/pages/dynamic/dynamic.component.ts
+++ src/app/pages/dynamic/dynamic.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { SeoService } from '../../services/seo.service';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-dynamic',
@@ -11,13 +12,13 @@ export class DynamicComponent implements OnInit {
   url: string;
 
   constructor(
-    @Inject('window') private window: Window,
+    @Inject(DOCUMENT) private document: Document,
     private router: Router,
     private seoService: SeoService,
   ){}
 
   ngOnInit(){
-    this.url = this.window.location.pathname;
+    this.url = this.document.location.pathname;
     if(this.url.indexOf('/not-found') === 0){
       this.router.navigate(['404'], {skipLocationChange: true});
     } else {
